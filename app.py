e9fb2ddd046287dae527633588278dcdb3683568
M.V.B.N SREEGANESH <163726114+klu2300031823@users.noreply.github.com>
2026-06-12 22:01:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -344,7 +344,7 @@ st.set_page_config(
 # -----------------------
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 # -----------------------
 # DOMAIN PROMPTS
